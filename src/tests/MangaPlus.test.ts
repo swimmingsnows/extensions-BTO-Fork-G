48b28d6a13ee9a4c1ea1dda86e0fe83240ff2bf4
GameFuzzy <fuzzy>
2021-09-01 21:47:31 +0200

    Fix TypeErrors

--- src/tests/MangaPlus.test.ts
+++ src/tests/MangaPlus.test.ts
@@ -41,15 +41,15 @@ describe('MangaPlus Tests', () => {
         expect(data, 'No chapters present for: [' + mangaId + ']').to.not.be.empty
 
         const entry = data[0]
-        expect(entry.id, 'No ID present').to.not.be.empty
-        expect(entry.name, 'No title available').to.not.be.empty
-        expect(entry.chapNum, 'No chapter number present').to.not.be.null
+        expect(entry?.id, 'No ID present').to.not.be.empty
+        expect(entry?.name, 'No title available').to.not.be.empty
+        expect(entry?.chapNum, 'No chapter number present').to.not.be.null
     })
 
     it('Get Chapter Details', async () => {
         const chapters = await wrapper.getChapters(source, mangaId)
         const chapter = chapters[0]
-        const data = await wrapper.getChapterDetails(source, mangaId, chapter.id)
+        const data = await wrapper.getChapterDetails(source, mangaId, chapters[0]?.id ?? 'unknown')
         console.log(chapter)
         console.log(data.pages.length)
         expect(data, 'No server response').to.exist
@@ -71,20 +71,20 @@ describe('MangaPlus Tests', () => {
 
         expect(result, 'No response from server').to.exist
 
-        expect(result.id, 'No ID found for search query').to.be.not.empty
-        expect(result.image, 'No image found for search').to.be.not.empty
-        expect(result.title, 'No title').to.be.not.null
-        expect(result.subtitleText, 'No subtitle text').to.be.not.null
+        expect(result?.id, 'No ID found for search query').to.be.not.empty
+        expect(result?.image, 'No image found for search').to.be.not.empty
+        expect(result?.title, 'No title').to.be.not.null
+        expect(result?.subtitleText, 'No subtitle text').to.be.not.null
     })
 
     it('Testing Home-Page aquisition', async () => {
         const homePages = await wrapper.getHomePageSections(source)
         expect(homePages, 'No response from server').to.exist
-        expect(homePages[0].items, 'No items present').to.exist
+        expect(homePages[0]?.items, 'No items present').to.exist
 
         // Ensure that we can resolve each of the images for the home-page, since these images are generated and not scraped
         const promises: Promise<void>[] = []
-        for (const obj of homePages[0].items ?? []) {
+        for (const obj of homePages[0]?.items ?? []) {
             promises.push(
                 axios.get(obj.image).then((imageResult: { status: any }) => {
                     expect(imageResult.status).to.equal(200)
