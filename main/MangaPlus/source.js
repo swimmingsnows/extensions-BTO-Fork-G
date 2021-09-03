77fb7ee6e984e5831013d50a893f001099805a73
GameFuzzy <30367459+GameFuzzy@users.noreply.github.com>
2021-09-03 18:19:59 +0000

    Deploying to gh-pages from @ GameFuzzy/extensions-gamefuzzy@4c205579cf17f94a60be1d87ab5ed1ad25dfc1bb 🚀

--- main/MangaPlus/source.js
+++ main/MangaPlus/source.js
@@ -8561,7 +8561,7 @@ class Parser {
     }
     filterSearchTitles(title, query) {
         var _a;
-        if (query.title.trim().length > 0 && query.parameters && query.parameters['author'] && query.parameters['author'][0]) {
+        if (query.title.trim().length > 0 && typeof query.parameters !== 'undefined' && typeof query.parameters['author'] !== 'undefined' && typeof query.parameters['author'][0] !== 'undefined') {
             return (title.name.toLowerCase()
                 .includes(query.title.toLowerCase()) &&
                 title.author
@@ -8569,7 +8569,7 @@ class Parser {
                     .includes(query.parameters['author'][0].toLowerCase()));
         }
         else {
-            if (query.parameters && query.parameters['author'] && query.parameters['author'][0]) {
+            if (typeof query.parameters !== 'undefined' && typeof query.parameters['author'] !== 'undefined' && typeof query.parameters['author'][0] !== 'undefined') {
                 return title.name
                     .toLowerCase()
                     .includes(query.title.toLowerCase()) ||
