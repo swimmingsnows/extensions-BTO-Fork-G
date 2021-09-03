fd055ab9bb7c0bcb355d7de0abaa009830c7ccda
GameFuzzy <fuzzy>
2021-09-03 16:46:29 +0200

    Try to fix TypeError

--- src/MangaPlus/Parser.ts
+++ src/MangaPlus/Parser.ts
@@ -158,7 +158,7 @@ export class Parser {
     }
 
     filterSearchTitles(title: Title, query: SearchRequest): boolean {
-        if (query.title!.trim().length > 0 && typeof query.parameters['author'] != 'undefined' && typeof query.parameters['author'][0] != 'undefined') {
+        if (query.title!.trim().length > 0 && query.parameters && query.parameters['author'] && query.parameters['author'][0]) {
             return (
                 title.name.toLowerCase()
                     .includes(query.title!.toLowerCase()) &&
@@ -168,7 +168,7 @@ export class Parser {
             )
         }
         else {
-            if (typeof query.parameters['author'] != 'undefined' && typeof query.parameters['author'][0] != 'undefined'){
+            if (query.parameters && query.parameters['author'] && query.parameters['author'][0]){
                 return title.name
                     .toLowerCase()
                     .includes(query.title!.toLowerCase()) ||
