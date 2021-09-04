swimmingsnows/extensions-BTO-Fork-G

commit 4ac42d68044f3a529a5292122929300bbff9cb1c

MangaPill - Fix tags whose IDs have whitespaces

src/MangaPill/MangaPill.ts
@@ -20,7 +20,7 @@ import { Parser } from './Parser'
 const MANGAPILL_DOMAIN = 'https://www.mangapill.com'
 
 export const MangaPillInfo: SourceInfo = {
-    version: '2.0.3',
+    version: '2.0.4',
     name: 'MangaPill',
     description: 'Extension that pulls manga from mangapill.com. It has a lot of officially translated manga but can sometimes miss manga notifications',
     author: 'GameFuzzy',
@@ -114,7 +114,6 @@ export class MangaPill extends Source {
                 ids: updatedManga.updates
             }))
         }
-
     }
 
     async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {

src/MangaPill/Parser.ts
@@ -172,7 +172,7 @@ export class Parser {
 
         for (const obj of $('.grid.gap-1 label').toArray()) {
             const label = $(obj).text().trim()
-            const id = '&genre=' + $('input', $(obj)).attr('value') ?? label
+            const id = `&genre=${$(obj).attr('value')}`.replace(' ', '+') ?? label
             tagSections[0]!.tags = [...tagSections[0]?.tags ?? [], createTag({id, label})]
         }
 
@@ -182,7 +182,7 @@ export class Parser {
             // Capitalize first letter
             label = label.charAt(0).toUpperCase() + label.slice(1)
 
-            const id = '&type=' + $(obj).attr('value') ?? label
+            const id = `&type=${$(obj).attr('value')}`.replace(' ', '+') ?? label
             tagSections[1]!.tags = [...tagSections[1]?.tags ?? [], createTag({id, label})]
         }
 
@@ -192,7 +192,7 @@ export class Parser {
             // Capitalize first letter
             label = label.charAt(0).toUpperCase() + label.slice(1)
 
-            const id = '&status=' + $(obj).attr('value') ?? label
+            const id = `&status=${$(obj).attr('value')}`.replace(' ', '+') ?? label
             tagSections[2]!.tags = [...tagSections[2]?.tags ?? [], createTag({id, label})]
         }
 

commit 09bd8af9eaafe62b3eeeddf8218a946b054635fb

MangaPill - Fix crash caused by 4ac42d6

src/MangaPill/MangaPill.ts
@@ -119,7 +119,7 @@ export class MangaPill extends Source {
     async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
         const page: number = metadata?.page ?? 1
         const tags: string = (query.includedTags
-            ?.map(tag => tag.id) ?? []).join('')
+            ?.map(tag => tag.id.replace(' ', '+')) ?? []).join('')
 
         const request = createRequestObject({
             url: `${MANGAPILL_DOMAIN}/search`,

src/MangaPill/Parser.ts
@@ -172,7 +172,7 @@ export class Parser {
 
         for (const obj of $('.grid.gap-1 label').toArray()) {
             const label = $(obj).text().trim()
-            const id = `&genre=${$(obj).attr('value')}`.replace(' ', '+') ?? label
+            const id = `&genre=${$(obj).attr('value')}` ?? label
             tagSections[0]!.tags = [...tagSections[0]?.tags ?? [], createTag({id, label})]
         }
 
@@ -182,7 +182,7 @@ export class Parser {
             // Capitalize first letter
             label = label.charAt(0).toUpperCase() + label.slice(1)
 
-            const id = `&type=${$(obj).attr('value')}`.replace(' ', '+') ?? label
+            const id = `&type=${$(obj).attr('value')}` ?? label
             tagSections[1]!.tags = [...tagSections[1]?.tags ?? [], createTag({id, label})]
         }
 
@@ -192,7 +192,7 @@ export class Parser {
             // Capitalize first letter
             label = label.charAt(0).toUpperCase() + label.slice(1)
 
-            const id = `&status=${$(obj).attr('value')}`.replace(' ', '+') ?? label
+            const id = `&status=${$(obj).attr('value')}` ?? label
             tagSections[2]!.tags = [...tagSections[2]?.tags ?? [], createTag({id, label})]
         }
 