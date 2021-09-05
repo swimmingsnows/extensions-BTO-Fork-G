aa7a01ff545bced09855bb67aa65829ec35735b4
GameFuzzy <30367459+GameFuzzy@users.noreply.github.com>
2021-09-05 00:27:26 +0000

    Deploying to gh-pages from @ GameFuzzy/extensions-gamefuzzy@d8a4352aaba61ed62c83ef3b8f0c513830012799 🚀

--- main/MangaPill/source.js
+++ main/MangaPill/source.js
@@ -612,7 +612,7 @@ const paperback_extensions_common_1 = require("paperback-extensions-common");
 const Parser_1 = require("./Parser");
 const MANGAPILL_DOMAIN = 'https://www.mangapill.com';
 exports.MangaPillInfo = {
-    version: '2.0.7',
+    version: '2.0.8',
     name: 'MangaPill',
     description: 'Extension that pulls manga from mangapill.com. It has a lot of officially translated manga but can sometimes miss manga notifications',
     author: 'GameFuzzy',
@@ -699,14 +699,14 @@ class MangaPill extends paperback_extensions_common_1.Source {
         });
     }
     getSearchResults(query, metadata) {
-        var _a, _b, _c;
+        var _a, _b, _c, _d;
         return __awaiter(this, void 0, void 0, function* () {
             const page = (_a = metadata === null || metadata === void 0 ? void 0 : metadata.page) !== null && _a !== void 0 ? _a : 1;
             const tags = ((_c = (_b = query.includedTags) === null || _b === void 0 ? void 0 : _b.map(tag => tag.id)) !== null && _c !== void 0 ? _c : []).join('');
             const request = createRequestObject({
                 url: `${MANGAPILL_DOMAIN}/search`,
                 method: 'GET',
-                param: encodeURI(`?q=${query.title}${tags}&page=${page}`)
+                param: encodeURI(`?q=${(_d = query.title) !== null && _d !== void 0 ? _d : ''}${tags}&page=${page}`)
             });
             const data = yield this.requestManager.schedule(request, 1);
             const $ = this.cheerio.load(data.data);
