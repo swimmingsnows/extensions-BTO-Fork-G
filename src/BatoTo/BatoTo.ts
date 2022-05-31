203c2f7b8b54c20f132cd708800b9669ff8624be
pandeynmn <namanpandey23@gmail.com>
2022-05-31 10:06:52 -0700

    Bato chapter page image fix
    Bato changed image url storage where the url to the image is stored in plain text and the token needed to successfully access the image is aes encrypted.

--- src/BatoTo/BatoTo.ts
+++ src/BatoTo/BatoTo.ts
@@ -18,7 +18,7 @@ import { Parser } from './Parser'
 const BATOTO_DOMAIN = 'https://bato.to'
 
 export const BatoToInfo: SourceInfo = {
-    version: '2.0.2',
+    version: '2.0.3',
     name: 'Bato.To',
     description: 'Extension that pulls western comics from bato.to',
     author: 'GameFuzzy & NmN',
