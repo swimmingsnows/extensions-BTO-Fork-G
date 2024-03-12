797271e49e005212c29b4287b8e03c3ada592cd1
swimmingsnows <160789332+swimmingsnows@users.noreply.github.com>
2024-03-12 06:35:14 -0700

    Update Parser.ts

--- src/BatoTo/Parser.ts
+++ src/BatoTo/Parser.ts
@@ -134,15 +134,13 @@ export class Parser {
                 const decryptScript = CryptoJS.AES.decrypt(encryptedToken, batoJS).toString(CryptoJS.enc.Utf8)
                 const tknArray = decryptScript.toString().replace(/"/g, '').replace(/[[\]']+/g,'', '').split(',')
                 if (imgArray != null) 
-
-                const pages = imgList.map((value: string, index: number) => `${value}?${tknList[index]}`)
-                   }
-                }
-            }
+                    const pages = imgList.map((value: string, index: number) => `${value}?${tknList[index]}`)
+               }
         }
+    }
+}
         return pages
     }
-
     filterUpdatedManga($: any, time: Date, ids: string[], source: any): { updates: string[], loadNextPage: boolean } {
         const foundIds: string[] = []
         let passedReferenceTime = false
